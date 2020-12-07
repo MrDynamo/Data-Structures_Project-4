d39658c1116b6af8c8d39382631355debd2c0ef8
MrDynamo <mrdynamomlg@gmail.com>
2020-12-06 21:15:28 -0600

    Cleaned up documentation.

--- src/com/github/mrdynamo/Project_4/TreeNode.java
+++ src/com/github/mrdynamo/Project_4/TreeNode.java
@@ -5,21 +5,16 @@ public class TreeNode<K extends Comparable<? super K>> extends KeyedItem<K> {
     TreeNode<K> leftChild; // Yes answer
     TreeNode<K> rightChild; // No answer
 
+    // Initializes tree node with item and no children.
     public TreeNode(K key) {
-        // Initializes tree node with item and no children.
         super(key);
-        //this.key = key;
-        //this.value = value;
         leftChild  = null;
         rightChild = null;
     }  // End constructor
 
+    // Initializes tree node with item and the left and right children references.
     public TreeNode(K key, TreeNode<K> left, TreeNode<K> right) {
-        // Initializes tree node with item and
-        // the left and right children references.
         super(key);
-        //this.key = key;
-        //this.value = value;
         leftChild  = left;
         rightChild = right;
     }  // End constructor
